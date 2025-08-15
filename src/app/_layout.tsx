b2dc4b06338f2f9a4369855bbd3b05194ae9b66e
lacerdamodtec <eduardo.lacerda@modernatec.com.br>
2025-08-15 20:37:19 -0300

    feat: config nativewindcss

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import "../global.css";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <Stack screenOptions={{ headerStyle: { backgroundColor: '#000' }, headerTintColor: '#FFD700', headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="details/[id]" options={{ title: 'Escolher' }}/>
+    </Stack>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from 'react-native';
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#111' }}>
+      <Text style={{ fontSize: 24, color: '#FFD700', marginBottom: 20 }}>🌌 Star Wars App</Text>
+
+        <Button title="Ver Luke Skywalker" />
+
     </View>
   );
 }
